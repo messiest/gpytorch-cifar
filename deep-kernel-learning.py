0cf88d463ffb4fb7a8303651f372e5eb1358b99c
Chris <messiercr@gmail.com>
2018-10-15 15:56:04 -0400

    returned call to likelihood() in test functionew

--- deep-kernel-learning.py
+++ deep-kernel-learning.py
@@ -110,8 +110,7 @@ def test():
     for data, target in test_loader:
         if CUDA: data, target = data.cuda(), target.cuda()  # no CUDA!
         with torch.no_grad():
-            # output = likelihood(model(data))
-            output = model(data)
+            output = likelihood(model(data))
             print("***" * 40)
             print('output')
             print(type(output))
